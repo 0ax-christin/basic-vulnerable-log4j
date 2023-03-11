a02a7f902c96b5e604d40f109ee92f922fda1649
Christin <caa8062@rit.edu>
2023-03-11 17:47:11 -0500

    ANother config, package compilation

--- src/main/java/com/heroes/api/heroesapi/HeroesApiApplication.java
+++ src/main/java/com/heroes/api/heroesapi/HeroesApiApplication.java
@@ -2,11 +2,15 @@ package com.heroes.api.heroesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class HeroesApiApplication extends SpringBootServletInitializer {
-
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+      return application.sources(HeroesApiApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(HeroesApiApplication.class, args);
 	}
