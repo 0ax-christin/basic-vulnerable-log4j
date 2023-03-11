708b919f29113435ac827866b3a2f7776ee31241
Christin <caa8062@rit.edu>
2023-03-11 16:11:06 -0500

    Getting ready for WAR file packaging for APache Tomcat deployment

--- src/main/java/com/heroes/api/heroesapi/HeroesApiApplication.java
+++ src/main/java/com/heroes/api/heroesapi/HeroesApiApplication.java
@@ -2,9 +2,10 @@ package com.heroes.api.heroesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HeroesApiApplication {
+public class HeroesApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeroesApiApplication.class, args);
