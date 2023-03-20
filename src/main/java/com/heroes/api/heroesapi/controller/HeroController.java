688d9d399d8515d5deba01c2fc1cadeec300f5a9
Christin <caa8062@rit.edu>
2023-03-20 00:07:39 -0400

    Created Hero Entity, Moved old hero model as HeroJSON.java

--- src/main/java/com/heroes/api/heroesapi/controller/HeroController.java
+++ src/main/java/com/heroes/api/heroesapi/controller/HeroController.java
@@ -17,7 +17,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.heroes.api.heroesapi.persistence.HeroDAO;
-import com.heroes.api.heroesapi.model.Hero;
+import com.heroes.api.heroesapi.model.HeroJSON;
 
 /**
  * Handles the REST API requests for the Hero resource
@@ -45,21 +45,21 @@ public class HeroController {
     }
 
     /**
-     * Responds to the GET request for a {@linkplain Hero hero} for the given id
+     * Responds to the GET request for a {@linkplain HeroJSON hero} for the given id
      * 
-     * @param id The id used to locate the {@link Hero hero}
+     * @param id The id used to locate the {@link HeroJSON hero}
      * 
-     * @return ResponseEntity with {@link Hero hero} object and HTTP status of OK if found<br>
+     * @return ResponseEntity with {@link HeroJSON hero} object and HTTP status of OK if found<br>
      * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
      * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @GetMapping("/{id}")
-    public ResponseEntity<Hero> getHero(@PathVariable int id) {
+    public ResponseEntity<HeroJSON> getHero(@PathVariable int id) {
         LOG.info("GET /heroes/" + id);
         try {
-            Hero hero = heroDao.getHero(id);
+            HeroJSON hero = heroDao.getHero(id);
             if (hero != null)
-                return new ResponseEntity<Hero>(hero,HttpStatus.OK);
+                return new ResponseEntity<HeroJSON>(hero,HttpStatus.OK);
             else
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -69,20 +69,20 @@ public class HeroController {
     }
 
     /**
-     * Responds to the GET request for all {@linkplain Hero heroes}
+     * Responds to the GET request for all {@linkplain HeroJSON heroes}
      * 
-     * @return ResponseEntity with array of {@link Hero hero} objects (may be empty) and
+     * @return ResponseEntity with array of {@link HeroJSON hero} objects (may be empty) and
      * HTTP status of OK<br>
      * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @GetMapping("")
-    public ResponseEntity<Hero[]> getHeroes(@RequestHeader("X-Api-Version") String apiVersion) {
+    public ResponseEntity<HeroJSON[]> getHeroes(@RequestHeader("X-Api-Version") String apiVersion) {
         LOG.error("Api version:" + apiVersion);
         // Replace below with your implementation
         try {
-            Hero[] heroes = heroDao.getHeroes();
+            HeroJSON[] heroes = heroDao.getHeroes();
             if(heroes.length != 0)
-                return new ResponseEntity<Hero[]>(heroes,HttpStatus.OK);
+                return new ResponseEntity<HeroJSON[]>(heroes,HttpStatus.OK);
         }
         catch(IOException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -91,12 +91,12 @@ public class HeroController {
     }
 
     /**
-     * Responds to the GET request for all {@linkplain Hero heroes} whose name contains
+     * Responds to the GET request for all {@linkplain HeroJSON heroes} whose name contains
      * the text in name
      * 
-     * @param name The name parameter which contains the text used to find the {@link Hero heroes}
+     * @param name The name parameter which contains the text used to find the {@link HeroJSON heroes}
      * 
-     * @return ResponseEntity with array of {@link Hero hero} objects (may be empty) and
+     * @return ResponseEntity with array of {@link HeroJSON hero} objects (may be empty) and
      * HTTP status of OK<br>
      * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      * <p>
@@ -104,14 +104,14 @@ public class HeroController {
      * GET http://localhost:8080/heroes/?name=ma
      */
     @GetMapping("/")
-    public ResponseEntity<Hero[]> searchHeroes(@RequestParam String name) {
+    public ResponseEntity<HeroJSON[]> searchHeroes(@RequestParam String name) {
         LOG.info("GET /heroes/?name="+name);
 
         // Replace below with your implementation
         try {
-            Hero[] heroes = heroDao.findHeroes(name);
+            HeroJSON[] heroes = heroDao.findHeroes(name);
             if(heroes.length != 0)
-                return new ResponseEntity<Hero[]>(heroes,HttpStatus.OK);
+                return new ResponseEntity<HeroJSON[]>(heroes,HttpStatus.OK);
         }
         catch(IOException e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -120,33 +120,33 @@ public class HeroController {
     }
 
     /**
-     * Creates a {@linkplain Hero hero} with the provided hero object
+     * Creates a {@linkplain HeroJSON hero} with the provided hero object
      * 
-     * @param hero - The {@link Hero hero} to create
+     * @param hero - The {@link HeroJSON hero} to create
      * 
-     * @return ResponseEntity with created {@link Hero hero} object and HTTP status of CREATED<br>
-     * ResponseEntity with HTTP status of CONFLICT if {@link Hero hero} object already exists<br>
+     * @return ResponseEntity with created {@link HeroJSON hero} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link HeroJSON hero} object already exists<br>
      * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @PostMapping("")
-    public ResponseEntity<Hero> createHero(@RequestBody Hero hero) {
+    public ResponseEntity<HeroJSON> createHero(@RequestBody HeroJSON hero) {
         LOG.info("POST /heroes " + hero);
 
         // Replace below with your implementation
         try{
-            Hero[] heroes = heroDao.getHeroes();
+            HeroJSON[] heroes = heroDao.getHeroes();
             int duplicate = 0;
             //Added functionality to check if user sends duplicate name and give status of CONFLICT if name already exists as per above documentation
             for(int i=0; i< heroes.length; i++) {
-                Hero heroListItem = heroes[i];
+                HeroJSON heroListItem = heroes[i];
                 if((hero.getName().contains(heroListItem.getName()) || (hero.getId() == heroListItem.getId())) == true) 
                     duplicate = 1;
             }
             
             if(duplicate == 0) {
-                Hero heroRep = heroDao.createHero(hero);
+                HeroJSON heroRep = heroDao.createHero(hero);
                 if(heroRep != null)
-                    return new ResponseEntity<Hero>(heroRep, HttpStatus.CREATED);
+                    return new ResponseEntity<HeroJSON>(heroRep, HttpStatus.CREATED);
             } else 
                 return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
@@ -157,23 +157,23 @@ public class HeroController {
     }
 
     /**
-     * Updates the {@linkplain Hero hero} with the provided {@linkplain Hero hero} object, if it exists
+     * Updates the {@linkplain HeroJSON hero} with the provided {@linkplain HeroJSON hero} object, if it exists
      * 
-     * @param hero The {@link Hero hero} to update
+     * @param hero The {@link HeroJSON hero} to update
      * 
-     * @return ResponseEntity with updated {@link Hero hero} object and HTTP status of OK if updated<br>
+     * @return ResponseEntity with updated {@link HeroJSON hero} object and HTTP status of OK if updated<br>
      * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
      * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @PutMapping("")
-    public ResponseEntity<Hero> updateHero(@RequestBody Hero hero) {
+    public ResponseEntity<HeroJSON> updateHero(@RequestBody HeroJSON hero) {
         LOG.info("PUT /heroes " + hero);
 
         // Replace below with your implementation
         try {
-            Hero updatedHero = heroDao.updateHero(hero);
+            HeroJSON updatedHero = heroDao.updateHero(hero);
             if(updatedHero != null)
-                return new ResponseEntity<Hero>(hero, HttpStatus.OK);    
+                return new ResponseEntity<HeroJSON>(hero, HttpStatus.OK);    
             else
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             
@@ -184,16 +184,16 @@ public class HeroController {
     }
 
     /**
-     * Deletes a {@linkplain Hero hero} with the given id
+     * Deletes a {@linkplain HeroJSON hero} with the given id
      * 
-     * @param id The id of the {@link Hero hero} to deleted
+     * @param id The id of the {@link HeroJSON hero} to deleted
      * 
      * @return ResponseEntity HTTP status of OK if deleted<br>
      * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
      * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @DeleteMapping("/{id}")
-    public ResponseEntity<Hero> deleteHero(@PathVariable int id) {
+    public ResponseEntity<HeroJSON> deleteHero(@PathVariable int id) {
         LOG.info("DELETE /heroes/" + id);
 
         // Replace below with your implementation
--- src/main/java/com/heroes/api/heroesapi/model/Hero.java
+++ src/main/java/com/heroes/api/heroesapi/model/Hero.java
@@ -1,60 +1,38 @@
 package com.heroes.api.heroesapi.model;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-/**
- * Represents a Hero entity
- * 
- */
+//https://www.baeldung.com/hibernate-identifiers
+//https://www.baeldung.com/jpa-entities
+
+@Entity
 public class Hero {
-    private static final Logger LOG = LogManager.getLogger(Hero.class.getName());
+    @Id
+    @GeneratedValue
+    private Integer id;
 
-    // Package private for tests
-    static final String STRING_FORMAT = "Hero [id=%d, name=%s]";
-
-    @JsonProperty("id") private int id;
-    @JsonProperty("name") private String name;
-
-    /**
-     * Create a hero with the given id and name
-     * @param id The id of the hero
-     * @param name The name of the hero
-     * 
-     * {@literal @}JsonProperty is used in serialization and deserialization
-     * of the JSON object to the Java object in mapping the fields.  If a field
-     * is not provided in the JSON object, the Java field gets the default Java
-     * value, i.e. 0 for int
-     */
-    public Hero(@JsonProperty("id") int id, @JsonProperty("name") String name) {
+    @Column(name="Hero_name", length=20, nullable=false, unique=false)
+    private String name;
+    
+    public Hero(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    /**
-     * Retrieves the id of the hero
-     * @return The id of the hero
-     */
-    public int getId() {return id;}
-
-    /**
-     * Sets the name of the hero - necessary for JSON object to Java object deserialization
-     * @param name The name of the hero
-     */
-    public void setName(String name) {this.name = name;}
-
-    /**
-     * Retrieves the name of the hero
-     * @return The name of the hero
-     */
-    public String getName() {return name;}
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public String toString() {
-        return String.format(STRING_FORMAT,id,name);
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
